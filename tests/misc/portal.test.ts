d76eca69f143eebc7a4bd67a44d44f92bb3a0129
Lucas Perais (lpe) <lpe@odoo.com>
2019-11-15 15:21:02 +0100

    WIP test

--- tests/misc/portal.test.ts
+++ tests/misc/portal.test.ts
@@ -1,21 +1,28 @@
 import { Portal } from "../../src/misc/portal";
 import { xml } from "../../src/tags";
-import { makeTestFixture, makeTestEnv } from "../helpers";
+import { makeTestFixture, makeTestEnv, nextTick } from "../helpers";
 import { Component } from "../../src/component/component";
+import { useState } from "../../src/hooks";
 
 //------------------------------------------------------------------------------
 // Setup and helpers
 //------------------------------------------------------------------------------
 
 // We create before each test:
-// - fixture: a div, appendde to the DOM, intended to be the target of dom
+// - fixture: a div, appended to the DOM, intended to be the target of dom
 //   manipulations.  Note that it is removed after each test.
+// - fixture has a child div with id #somewhere-else meant to be used as
+//   target by Portal component
 // - a test env, necessary to create components, that is set on Component
 
 let fixture: HTMLElement;
 
 beforeEach(() => {
   fixture = makeTestFixture();
+  const div = document.createElement("div");
+  div.setAttribute("id", "somewhere-else");
+  fixture.appendChild(div);
+
   Component.env = makeTestEnv();
 });
 
@@ -25,20 +32,15 @@ afterEach(() => {
 
 describe("Portal", () => {
   test("basic use of portal", async () => {
-    // TODO: move this in beforeEach?
-    const div = document.createElement("div");
-    div.setAttribute("id", "somewhere-else");
-    fixture.appendChild(div);
-
     class Parent extends Component<any, any> {
       static components = { Portal };
       static template = xml`
-          <div>
-            <span>in parent</span>
-            <Portal target="'#somewhere-else'">
-              <span>somewhere else</span>
-            </Portal>
-          </div>`;
+        <div>
+          <span>in parent</span>
+          <Portal target="'#somewhere-else'">
+            <span>somewhere else</span>
+          </Portal>
+        </div>`;
     }
 
     const parent = new Parent();
@@ -46,4 +48,30 @@ describe("Portal", () => {
 
     expect(fixture.innerHTML).toMatchSnapshot();
   });
+
+  test("conditional use of Portal", async () => {
+    class Parent extends Component<any, any> {
+      static components = { Portal };
+      static template = xml`
+        <div>
+          <span>in parent</span>
+          <Portal target="'#somewhere-else'" t-if="state.hasPortal">
+            <span>somewhere else</span>
+          </Portal>
+        </div>`;
+
+      state = useState({ hasPortal: false });
+    }
+
+
+    const parent = new Parent();
+    await parent.mount(fixture);
+
+    expect(fixture.innerHTML).toMatchSnapshot();
+    parent.state.hasPortal = true;
+    await nextTick();
+    expect(fixture.innerHTML).toMatchSnapshot();
+  });
+
+
 });
